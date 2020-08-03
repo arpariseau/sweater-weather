a7a20a3d486c76d266c725330c6aa510b10722b7
Alex Pariseau <arpariseau@gmail.com>
2020-08-03 11:58:16 -0600

    Tests running smoothly

--- spec/requests/forecasts/get_forecast_spec.rb
+++ spec/requests/forecasts/get_forecast_spec.rb
@@ -4,6 +4,11 @@ describe 'an api request' do
   it 'can get forecast' do
     get api_v1_forecast_path, params: { location: "Denver,CO" }
     forecast_resp = JSON.parse(response.body, symbolize_names: true)[:data]
-    binding.pry
+
+    forecast = forecast_resp[:attributes]
+    expect(forecast).to have_key :location
+    expect(forecast).to have_key :current
+    expect(forecast).to have_key :daily
+    expect(forecast).to have_key :hourly
   end
 end
