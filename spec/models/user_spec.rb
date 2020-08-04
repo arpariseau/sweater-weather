3820f514ef53a8f59da8ca2ddaee62318e6deee1
Alex Pariseau <arpariseau@gmail.com>
2020-08-04 16:07:00 -0600

    User can now register

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:password_confirmation) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_confirmation_of(:password) }
+    it { should have_secure_password }
+  end
+end
---
+++ spec/requests/users/post_create_user_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe 'an api request' do
+  it 'can create a user' do
+    post_body = { "email": "whatever@example.com",
+                  "password": "password",
+                  "password_confirmation": "password" }
+    post api_v1_users_path, params: post_body, as: :json
+    expect(response.status).to eq(201)
+    user_resp = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(user_resp[:type]).to eq("user")
+    expect(user_resp).to have_key :id
+
+    user_attr = user_resp[:attributes]
+    expect(user_attr[:email]).to eq("whatever@example.com")
+    expect(user_attr[:api_key]).to_not be_empty
+  end
+
+  it 'gives an error if email is not unique' do
+    post_body = { "email": "whatever@example.com",
+                  "password": "password",
+                  "password_confirmation": "password" }
+    post api_v1_users_path, params: post_body, as: :json
+    expect(response.status).to eq(201)
+
+    replica_post_body = { "email": "whatever@example.com",
+                          "password": "password",
+                          "password_confirmation": "password" }
+    post api_v1_users_path, params: replica_post_body, as: :json
+    expect(response.status).to eq(400)
+    error_resp = JSON.parse(response.body, symbolize_names: true)
+
+    expect(error_resp[:error]).to eq("Email has already been taken")
+  end
+
+  it "gives an error when password doesn't match confirmation" do
+    post_body = { "email": "whatever@example.com",
+                  "password": "password",
+                  "password_confirmation": "notpassword" }
+    post api_v1_users_path, params: post_body, as: :json
+    expect(response.status).to eq(400)
+    error_resp = JSON.parse(response.body, symbolize_names: true)
+
+    expect(error_resp[:error]).to eq("Password confirmation doesn't match Password")
+  end
+
+  it "gives an error when one of the information pieces is blank" do
+    post_body = { "email": "whatever@example.com",
+                  "password": "password" }
+    post api_v1_users_path, params: post_body, as: :json
+    expect(response.status).to eq(400)
+    error_resp = JSON.parse(response.body, symbolize_names: true)
+
+    expect(error_resp[:error]).to eq("Password confirmation can't be blank")
+  end
+end
